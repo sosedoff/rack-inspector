aeb121477a8fac288c218f52dbab78910f66e4d2
Dan Sosedoff <dan.sosedoff@gmail.com>
2013-09-16 21:28:27 -0500

    Refactor payload

--- lib/rack/inspector/payload.rb
+++ lib/rack/inspector/payload.rb
@@ -2,35 +2,45 @@ module Rack
   class Inspector
     class Payload
       def initialize(middleware, request, status, headers, body)
-        @hash = {
-          id:             SecureRandom.uuid,
-          app:            middleware.name,
-          host:           middleware.hostname,
-          request_method: request.request_method,
-          path:           request.path_info,
-          status:         status,
-          timestamp:      Time.now.utc,
-
-          request: {
-            query_string:   request.query_string,
-            params:         request.params,
-            body:           request.body.read,
-            env:            select_env(request.env)
-          },
-
-          response: {
-            status:  status,
-            headers: headers,
-            body:    response_body(body)
-          }
-        }
+        @middleware = middleware
+        @request    = request
+        @status     = status
+        @headers    = headers
+        @body       = body
       end
 
       def to_hash
-        @hash
+        {
+          id:             SecureRandom.uuid,
+          app:            @middleware.name,
+          host:           @middleware.hostname,
+          request_method: @request.request_method,
+          path:           @request.path_info,
+          status:         @status,
+          timestamp:      Time.now.utc,
+          request:        request_hash,
+          response:       response_hash
+        }
       end
 
       private
+
+      def request_hash
+        {
+          query_string:   @request.query_string,
+          params:         @request.params,
+          body:           @request.body.read,
+          env:            select_env(@request.env)
+        }
+      end
+
+      def response_hash
+        {
+          status:  @status,
+          headers: @headers,
+          body:    response_body(@body)
+        }
+      end
 
       def select_env(env)
         env.select { |_,v| v.kind_of?(String) }
